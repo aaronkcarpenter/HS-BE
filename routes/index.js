a0ed7e55bb1dc193cbbd0b5ea65cc5ea55f40a1c
Aaron <aaronkcarpenter@gmail.com>
2020-10-08 12:34:32 -0500

    index.js code deletion

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const api = require('./api');
 
-router.get("/", (req, res) => {
-  res.send("index root");
-});
+// router.get("/", (req, res) => {
+//   res.send("index root");
+//   res.render('./')
+// });
+
+router.use('/api', api)
 
 module.exports = router;
