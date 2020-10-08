83157c11d0aba359e1c9142bae07a873b825aa64
Aaron <aaronkcarpenter@gmail.com>
2020-10-08 16:49:40 -0500

    testing express server

--- routes/api/login.js
+++ routes/api/login.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
 
-const { User } = require('../../db/models');
+const { UserTable } = require('../../db/models');
 
 const router = express.Router();
 
 router.get('/', asyncHandler(async function (req, res, next) {
-  const users = await User.findAll();
+  const users = await UserTable.findAll();
   res.json({ users });
 }));
 
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
 
-const { User } = require('../../db/models');
+const { Usertable } = require('../../db/models');
 
 const router = express.Router();
 
 router.get('/', asyncHandler(async function (req, res, next) {
-  const users = await User.findAll();
+  const users = await Usertable.findAll();
   res.json({ users });
 }));
 
